7b946d1df6e6722dcdbb266622949fc22f91bdf6
Safia Abdale <safia.abdale@gmail.com>
2021-11-08 04:27:19 +0000

    Use a function avoid duplicating code

---
+++ hello.js
@@ -0,0 +1,19 @@
+// Declaring and Calling Functions
+
+// Basic function:
+// const sayHello  = function () {
+//   console.log("Hello, world");
+// }
+
+// sayHello();
+
+
+
+// Improved function:
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+// Testing
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
