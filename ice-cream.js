bf35d4960582c00f84d82534399be8f808ea9122
Safia Abdale <safia.abdale@gmail.com>
2021-11-14 05:00:42 +0000

    Array exercises

---
+++ ice-cream.js
@@ -0,0 +1,14 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+// 1. Add a flavour - Push root beer
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+// 2. Get the first flavour
+console.log(iceCreamFlavours[0]);
+// 3. Get the last flavour
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+// 4. Total number of flavours
+console.log(iceCreamFlavours.length);
--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,17 @@
-const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+// Iterating over an array with For:
+/*const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}*/
+
+
+
+// Iterating over an array with While:
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
